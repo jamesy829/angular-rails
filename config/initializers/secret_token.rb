7d2632f503c4a97e6ce5a44771b34279aeba584f
James Huang <jamesy.huang@hotmail.com>
2014-02-16 12:40:44 -0500

    Create rails project with postgresql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AngularRails::Application.config.secret_key_base = '41b7952eb4c8567a3b2ccf3f8a95551efbbe4f5a2fad9a220e31d15ef5fdae81470b65ed73d939dda13a16b4afe269bbebb42498fde0f93739e0d5ea33ddc075'
